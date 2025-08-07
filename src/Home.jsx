954b4bdfa4ca08b9a86fc93d0fb929e378d2c6b3
Subramaniya Jothi <subramaniyajothi2002@gmail.com>
2025-08-07 13:58:52 +0530

    initial commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4">
         <div className="container-fluid container">
           <Link className="navbar-brand" to="/">Password Reset Flow</Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+          <button className="navbar-toggler" aria-controls="navbarNav" aria-label="Toggle navigation" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span className="navbar-toggler-icon"></span>
           </button>
 
